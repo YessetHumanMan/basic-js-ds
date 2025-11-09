511215ed2c52f1a2d943ac6c0eb84c939665ca59
YessetHumanMan <eset.bimanov@gmail.com>
2025-11-09 13:22:27 +0500

    feat run true tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,44 +1,84 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this._root = null;
+  }
+
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this._root;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    const insert = (node, data) => {
+      if (!node) return new Node(data);
+      if (data === node.data) return node; // не вставляем дубли
+      if (data < node.data) node.left = insert(node.left, data);
+      else node.right = insert(node.right, data);
+      return node;
+    };
+    this._root = insert(this._root, data);
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    let cur = this._root;
+    while (cur) {
+      if (data === cur.data) return cur;
+      cur = data < cur.data ? cur.left : cur.right;
+    }
+    return null;
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return !!this.find(data);
   }
 
-  remove(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    const removeNode = (node, data) => {
+      if (!node) return null;
+
+      if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      }
+      if (data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+
+      // data === node.data
+      if (!node.left && !node.right) return null; // лист
+      if (!node.left) return node.right;          // только правый
+      if (!node.right) return node.left;          // только левый
+
+      // два ребёнка — берем минимальный в правом поддереве
+      let min = node.right;
+      while (min.left) min = min.left;
+      node.data = min.data;
+      node.right = removeNode(node.right, min.data);
+      return node;
+    };
+
+    this._root = removeNode(this._root, data);
   }
 
   min() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this._root) return null;
+    let n = this._root;
+    while (n.left) n = n.left;
+    return n.data;
   }
 
   max() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this._root) return null;
+    let n = this._root;
+    while (n.right) n = n.right;
+    return n.data;
   }
 }
 
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -4,28 +4,23 @@ const { NotImplementedError } = require('../lib/errors');
 /**
  * Given a singly linked list of integers l and an integer k,
  * remove all elements from list l that have a value equal to k.
- *
- * @param {List} l
- * @param {Number} k
- * @return {List}
- *
- * @example
- * For l = [3, 1, 2, 3, 4, 5] and k = 3,
- * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
  */
-function removeKFromList(/* l, k */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function removeKFromList(l, k) {
+  // убираем головы, равные k
+  while (l && l.value === k) l = l.next;
+
+  let cur = l;
+  while (cur && cur.next) {
+    if (cur.next.value === k) {
+      cur.next = cur.next.next;
+    } else {
+      cur = cur.next;
+    }
+  }
+  return l;
 }
 
 module.exports = {
   removeKFromList
 };
+
